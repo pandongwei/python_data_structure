0f4aea75e5c848833cfa3300ac32ea400a6733dd
usrki <usrki@student.kit.edu>
2020-09-21 17:48:55 +0800

    update

--- sort.py
+++ sort.py
@@ -1,4 +1,4 @@
-# 冒泡排序
+# 冒泡排序， O(n2)
 def BubbleSort(nums):
     l = len(nums)
     for i in range(l):
@@ -7,7 +7,7 @@ def BubbleSort(nums):
                 nums[j-1], nums[j] = nums[j], nums[j-1]
     return nums
 
-# 选择排序
+# 选择排序， O(n2)
 def SelectSort(nums):
     l = len(nums)
     for i in range(l):
@@ -19,7 +19,7 @@ def SelectSort(nums):
             nums[i], nums[min] = nums[min], nums[i]
     return nums
 
-# 插入排序
+# 插入排序， O(n2)
 def InsertSort(nums):
     l = len(nums)
     for i in range(1, l):
@@ -32,108 +32,138 @@ def InsertSort(nums):
             nums[j+1] = tmp
     return nums
 
-# 希尔排序
-def ShellSort(nums): #TODO
-    l = len(nums)
-    increment = l
-    acc = 0
-    while increment >=1 and acc == 0:
-        increment = increment // 3 + 1
-        if increment == 1:
-            acc = 1
-        for i in range(increment, l):
-            if nums[i] < nums[i-increment]:
-                tmp = nums[i]
-                j = i - increment
-                while j >= 0 and nums[j] > tmp:
-                    nums[j+increment] = nums[j]
-                    j -= increment
-                nums[j+increment] = tmp
-    return nums
-
-
+# 希尔排序，插入排序的升级版，O(nlogn)
 def shellSort(nums):
-    n = len(nums)
-    gap = int(n / 2)
-    while gap > 0:
-        for i in range(gap, n):
-            temp = nums[i]
+    l = len(nums)
+    if l==1 or l==0: return nums
+    increment = l // 2
+    while increment >= 1:
+        for i in range(increment, l):
             j = i
-            while j >= gap and nums[j - gap] > temp:
-                nums[j] = nums[j - gap]
-                j -= gap
-            nums[j] = temp
-        gap = int(gap / 2)
+            while j >= increment and nums[j] < nums[j-increment]:
+                nums[j], nums[j-increment] = nums[j-increment], nums[j]
+                j -= increment
+        increment //= 2
     return nums
 
-# 堆排序
+# 堆排序 O(nlogn)
+# 数组创建一个最大堆；开始迭代，每次堆首与堆尾互换，堆尺寸缩小1，并且再次调整位置形成最大堆
+# 重复上述步骤直至结束
 def HeapSort(nums):
+    l = len(nums)
+    def heapify(nums, i):
+        left, right, largest = 2*i+1, 2*i+2, i
+        if left < l and nums[left] > nums[largest]:
+            largest = left
+        if right < l and nums[right] > nums[largest]:
+            largest = right
+        if largest != i:
+            nums[i], nums[largest] = nums[largest], nums[i]
+            heapify(nums, largest)
+    def buildMaxHeap(nums):
+        for i in range(len(nums)//2, -1, -1):
+            heapify(nums, i)
+    buildMaxHeap(nums)
+    for i in range(len(nums)-1, 0, -1):
+        nums[0], nums[i] = nums[i], nums[0]
+        l -= 1
+        heapify(nums, 0)
+    return nums
+
+# 归并排序 递归 O(nlogn)
+# 把数组分为多个小数组排序，然后往上合并，每两个有序数组进行合并
+def mergeSort_digui(nums):
+    def merge(nums_l, nums_r):
+        res = []
+        while nums_l and nums_r:
+            if nums_l[0] <= nums_r[0]:
+                res.append(nums_l.pop(0))
+            else:
+                res.append(nums_r.pop(0))
+        while nums_l:
+            res.append(nums_l.pop(0))
+        while nums_r:
+            res.append(nums_r.pop(0))
+        return res
+    l = len(nums)
+    if l < 2: return nums
+    mid = l//2
+    nums_l, nums_r = nums[:mid], nums[mid:]
+    return merge(mergeSort_digui(nums_l), mergeSort_digui(nums_r))
+
+# 归并排序 迭代
+def mergeSort_diedai(nums):
     pass
 
-# 归并排序
-def merge_sort(a: List[int]):
-    _merge_sort_between(a, 0, len(a) - 1)
+# def QuickSort(nums):
+#     def QSort(nums, low, high):
+#         if low < high:
+#             pivot = Partition(nums, low, high)
+#             QSort(nums, low, pivot-1)
+#             QSort(nums, pivot+1, high)
+#     def Partition(nums, low, high):
+#         pivotkey = nums[low]
+#         while low < high:
+#             while low < high and nums[high] >= pivotkey:
+#                 high -= 1
+#             nums[low], nums[high] = nums[high], nums[low]
+#             while low < high and nums[low] <= pivotkey:
+#                 low += 1
+#             nums[low], nums[high] = nums[high], nums[low]
+#         return low
+#     l = len(nums)
+#     QSort(nums, 0, l-1)
+#     return nums
 
-def _merge_sort_between(a: list[int], low: int, high: int):
-    # The indices are inclusive for both low and high.
-    if low < high:
-        mid = low + (high - low) // 2
-        _merge_sort_between(a, low, mid)
-        _merge_sort_between(a, mid + 1, high)
-        _merge(a, low, mid, high)
-
-def _merge(a: list[int], low: int, mid: int, high: int):
-    # a[low:mid], a[mid+1, high] are sorted.
-    i, j = low, mid + 1
-    tmp = []
-    while i <= mid and j <= high:
-        if a[i] <= a[j]:
-            tmp.append(a[i])
-            i += 1
-        else:
-            tmp.append(a[j])
-            j += 1
-    start = i if i <= mid else j
-    end = mid if i <= mid else high
-    tmp.extend(a[start:end + 1])
-    a[low:high + 1] = tmp
-
-def QuickSort(nums):
-    def QSort(nums, low, high):
-        if low < high:
-            pivot = Partition(nums, low, high)
-            QSort(nums, low, pivot-1)
-            QSort(nums, pivot+1, high)
-    def Partition(nums, low, high):
-        pivotkey = nums[low]
-        while low < high:
-            while low < high and nums[high] >= pivotkey:
-                high -= 1
-            nums[low], nums[high] = nums[high], nums[low]
-            while low < high and nums[low] <= pivotkey:
-                low += 1
-            nums[low], nums[high] = nums[high], nums[low]
-        return low
-    l = len(nums)
-    QSort(nums, 0, l-1)
+# 快速排序，O(nlogn)
+def quickSort(nums):
+    def Qsort(nums, left, right):
+        if left >= right: return
+        low, high = left, right
+        pivot = nums[low]
+        while left < right:
+            while left < right and pivot <= nums[right]:
+                right -= 1
+            nums[left] = nums[right]
+            while left < right and pivot >= nums[left]:
+                left += 1
+            nums[right] = nums[left]
+        nums[left] = pivot
+        Qsort(nums,low,left-1)
+        Qsort(nums,left+1,high)
+    Qsort(nums,0,len(nums)-1)
     return nums
 
-def quick_sort(nums,left,right):
-    if left >= right: return
-    low, high = left, right
-    pivotkey = nums[low]
-    while left < right:
-        while left < right and nums[right] > pivotkey:
-            right -= 1
-        nums[left] = nums[right]
-        while left < right and nums[left] <= pivotkey:
-            left += 1
-        nums[right] = nums[left]
-    nums[right] = pivotkey
-    quick_sort(nums, low, left-1)
-    quick_sort(nums, left+1, right)
+# 桶排序 O(n+k)
+def bucketSort(nums):
+    pass
 
+# 计数排序，O(n+k),k为数组的范围
+def countingSort(nums):
+    l = len(nums)
+    min, max = nums[0], nums[0]
+    for i in range(1, l):
+        if nums[i] < min: min = nums[i]
+        if nums[i] > max: max = nums[i]
+    bucket = [0 for i in range(max-min+1)]
+    for i in range(l):
+        bucket[nums[i]-min] += 1
+    j = 0
+    for i in range(len(bucket)):
+        while bucket[i] > 0:
+            nums[j] = i+min
+            j += 1
+            bucket[i] -= 1
+    return nums
+
+# 基数排序,O(kn),k为位数通常很小
+# 将所有数值统一为同样的数位长度，需要补零
+# 从最低位开始，根据该位的数进行排序（需要建立10个桶，从0-9）
+# 历遍至最高位，则完成排序
+def indexSort(nums):
+    pass
 
 test = [10,9,8,7,6,5,4,3,2,1,0]
-test1 = [8,6,4,7,9,2,1,3,10,5,0]
-print(QuickSort(test))
+test1 = [8,8,4,7,1,2,1,3,10,5,0]
+HeapSort(test)
+print(test)
